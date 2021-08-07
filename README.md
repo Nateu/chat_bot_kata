360b839511050343837c3be49a1b9cbc5d1bfee0
Pascal Clarkson <info@artocraft.nl>
2021-08-07 12:47:53 +0000

    Failure: Given a CommandParser when a message is 'thEme' it should ignore case

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 To run:
 
 unit tests
-* hit the __run__ button or
-* run __mamba --format=documentation specs/*_spec.py__ in the shell
+* click the __run__ button in replit
+
+Or in a shell
+* __mamba --format=documentation specs/*\_spec.py__
 
 the bot in the shell tab:
 * __python bot__
--- specs/command_parser_spec.py
+++ specs/command_parser_spec.py
@@ -2,37 +2,50 @@ from mamba import description, context, it, before
 from expects import expect, equal
 from bot import command_parser
 
-'''
-Feature 1:
-The bot should be able to parse a message for a command 
-to retrieve and set a theme of the day.
-'''
-
 with description("Given a CommandParser") as self:
-    with context("When a message is 'Theme'"):
+    with before.each:
+        self.command_parser = command_parser.CommandParser()
+
+    '''
+    Feature 1:
+    The bot should be able to parse a message for a command 
+    to retrieve and set a theme of the day.
+    '''
+
+    with context("when a message is 'Theme'"):
         with it("should return 'No theme set.'"):
-            my_command_parser = command_parser.CommandParser()
             expect(
-                my_command_parser.parse("Theme")
+                self.command_parser.parse("Theme")
             ).to(
                 equal("No theme set.")
             )
 
-    with context("When a message starts with 'Set theme'"):
+    with context("when a message starts with 'Set theme'"):
         with it("should store the remainder of the message as the theme"):
-            my_command_parser = command_parser.CommandParser()
             expect(
-                my_command_parser.parse("Set theme Disney movie songs")
+                self.command_parser.parse("Set theme Disney movie songs")
             ).to(
                 equal("Disney movie songs")
             )
 
-    with context("When a theme is set"):
+    with context("when a theme is set"):
         with it("should remember it a moment later"):
-            my_command_parser = command_parser.CommandParser()
-            my_command_parser.parse("Set theme Disney movie songs")
+            self.command_parser.parse("Set theme Disney movie songs")
             expect(
-                my_command_parser.parse("Theme")
+                self.command_parser.parse("Theme")
             ).to(
                 equal("Disney movie songs")
+            )
+
+    '''
+    Feature 2:
+    The bot should handle commands ignoring upper/lower case differences.
+    '''
+
+    with context("when a message is 'thEme'"):
+        with it("it should ignore case"):
+            expect(
+                self.command_parser.parse("thEme")
+            ).to(
+                equal("No theme set.")
             )
